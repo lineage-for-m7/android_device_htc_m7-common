AICP/device_htc_m7-common

m7-common: import shims from https://github.com/htc-msm8960-dev
Change-Id: I1a903b6bf9125f81806da9231d4fb264e6c9460c

libshims/Android.mk
@@ -27,10 +27,61 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    wvm_shim.cpp
+    vcsfp_shim.cpp
 
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE := libvcsfp_shim
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := bionic.cpp
+LOCAL_MODULE := libshims_bionic
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_32_BIT_ONLY := true
+LOCAL_SHARED_LIBRARIES := libc
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    ril_shim.cpp
+LOCAL_MODULE := libshims_ril
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    atomic.cpp
+
+LOCAL_MODULE := libshims_atomic
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+	pthread_kill.cpp
+
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_SHARED_LIBRARIES := libdl
+LOCAL_MODULE := libC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    icu53.cpp \
+    libqc-opt_shim.c
+
+LOCAL_SHARED_LIBRARIES := libicuuc libicui18n liblog
+LOCAL_MODULE := libshim_qcopt
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)

lineage-for-m7/android_device_htc_m7-common

libshims/Android.mk
@@ -27,10 +27,60 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    wvm_shim.cpp
+    vcsfp_shim.cpp
 
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE := libvcsfp_shim
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := bionic.cpp
+LOCAL_MODULE := libshims_bionic
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_32_BIT_ONLY := true
+LOCAL_SHARED_LIBRARIES := libc
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    ril_shim.cpp
+LOCAL_MODULE := libshims_ril
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    atomic.cpp
+
+LOCAL_MODULE := libshims_atomic
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+	pthread_kill.cpp
+
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_SHARED_LIBRARIES := libdl_android
+LOCAL_MODULE := libC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    GraphicBuffer.cpp
+
+LOCAL_SHARED_LIBRARIES := libui
+LOCAL_MODULE := libui_shim
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)