AICP/device_htc_m7-common

m7-common: Create media_profiles_V1_0.xml
media_profiles_V1_0.xml is created from media_profiles.xml by replacing
the dtd schema inside media_profiles.xml with an external reference to
/system/etc/media_profiles_V1_0.dtd.

Change-Id: I0fbb03603a72d0108a96bfa0f0a4ceb12ef3d430

m7-common.mk
@@ -87,7 +87,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # NFC
 PRODUCT_PACKAGES += \

lineage-for-m7/android_device_htc_m7-common

m7-common.mk
@@ -87,7 +87,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # NFC
 PRODUCT_PACKAGES += \